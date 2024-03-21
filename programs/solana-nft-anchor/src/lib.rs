9672a60cab1a6f36f38eb65b5489dedb551d96f1
xabimolana <164369840+xabimolana@users.noreply.github.com>
2024-03-21 22:00:06 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("7becqvTsYx2mWG4GkY5yPyF5Ctizq91F95eP9nGGdELy");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
